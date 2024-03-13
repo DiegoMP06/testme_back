bcf979a746d0c84a612b31bf8a33d33c6a332fbe
Diego Meneses Perez <perezdi060606@gmail.com>
2024-03-12 18:21:01 -0600

    Resolver Bugs de Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,14 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::get('/dashboard', [AuthenticatedSessionController::class, 'destroy'])->name('dashboard');
+Route::get('/dashboard', function (Request $request) {
+    Auth::guard('web')->logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return response(null, 404);
+})->name('dashboard');
 
 require __DIR__.'/auth.php';
